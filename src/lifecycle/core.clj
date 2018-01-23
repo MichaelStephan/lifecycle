dad8990ca4198e6ddba959959e69c6efcc6a9cca
Michael Stephan <michael.stephan@sap.com>
2018-01-23 17:09:27 +0100

    update

--- src/lifecycle/core.clj
+++ src/lifecycle/core.clj
@@ -1,45 +1,41 @@
 (ns lifecycle.core
   (:require [clojure.set :as s]))
 
-(def tas-changelog [{:meta {:id "hana"
-                            :ver 1
-                            :gen 1}
-                     :data {:marketing {}
-                            :variants {}
-                            :metrics {}}}])
-
-(defn append-to-changelog [change-log id-obj data]
-  (conj change-log (merge (update id-obj :ver inc) {:data data})))
+(defn append-to-changelog [changelog entity-id data]
+  (conj changelog (merge entity-id {:data data})))
 
 (defn change-type [cmd location]
-  (if (empty? (s/intersection (apply hash-set location) #{:variants}))
+  (if (empty? (s/intersection (apply hash-set location) #{:variants :metrics}))
     :non-breaking
     :breaking))
 
-(defn change [change-log {:keys [id ver] :as obj-id} changes options]
-  (let [[[cmd location data :as change] & rest] changes]
-    (if change
-      (let [existing-obj (filter (fn [{{existing-meta-id :id
-                                        existing-meta-ver :ver} :meta}]
-                                   (and (= existing-meta-id id)
-                                        (= existing-meta-ver ver)))
-                                 change-log)
-            change-type (change-type cmd location)]
-        (if (or (contains? options :ignore-breaking-change)
-                (= :non-breaking change-type))  
-          (recur (condp = cmd
-                   :update (append-to-changelog change-log obj-id (assoc-in {} location data))
-                   :delete (append-to-changelog change-log obj-id (assoc-in {} location nil)))
-                 obj-id rest options)
-          {:error {:type :breaking-change-violation}}))
-      change-log)))
+(defn get-from-changelog [changelog {id-a :id major-a :major minor-a :minor :or {major 0 minor 0}}]
+  (filter (fn [{id-b :id major-b :major minor-b :minor}]
+            (and (= id-a id-b)
+                 (if major-a (= major-a major-b) true)
+                 (if minor-a (= minor-a minor-b) true)))
+          changelog))
 
-(->
- (change tas-changelog {:id "hana" :ver 1}
-         [[:update [:marketing :name] "hugo"]
-         [:update [:marketing :description] "this is hugo"]
-         [:update [:variants] 2]
-         [:delete [:marketing :name]]]
-         #{:ignore-breaking-change})
- (clojure.pprint/pprint))
+(defn exists-in-changelog? [changelog entity-id]
+  (not (empty? (get-from-changelog changelog entity-id))))
 
+(defn increment-minor [entity-id]
+  (update entity-id :minor inc))
+
+(defn increment-major-and-minor [entity-id]
+  (update (increment-minor entity-id) :major inc))
+
+(defn change [changelog entity-id [cmd location data :as change] & options]
+  (let [entity-id (merge {:major 1 :minor 0} entity-id)
+        new-entity-id (condp = (change-type cmd location)
+                        :non-breaking (increment-minor entity-id)
+                        :breaking (increment-major-and-minor entity-id))]
+    (if (exists-in-changelog? changelog new-entity-id)
+      (throw (ex-info "optimistic lock violation" {}))
+      (append-to-changelog changelog new-entity-id (assoc-in {} location (condp = cmd
+                                                                           :update data
+                                                                           :delete nil))))))
+(-> []
+    (change {:id "hana"} [:update [:name] "hugo"])
+    (change {:id "hana" :minor 1} [:update [:name] "hugo2"])
+    (clojure.pprint/pprint))
--- test/lifecycle/core_test.clj
+++ test/lifecycle/core_test.clj
@@ -2,6 +2,29 @@
   (:require [clojure.test :refer :all]
             [lifecycle.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-get-from-changelog
+  (let [not-existing-entity-id {:id 0 :major 0 :minor 0}
+        existing-entity-id {:id "hana" :major 1 :minor 1}
+        change-log [{:id "hana" :major 1 :minor 1 :data {:a 1}}
+                    {:id "hana" :major 1 :minor 2 :data {:a 2}}
+                    {:id "hana" :major 2 :minor 1 :data {:a 2 :b 1}}
+                    {:id "hana" :major 2 :minor 2 :data {:a 2 :b 2}}
+                    {:id "redis" :major 1 :minor 1 :data {:a 1}}]]
+    (testing "try to get not-existing entry from an empty changelog"
+      (is (empty? (get-from-changelog [] not-existing-entity-id))))
+    (testing "try to get not-existing entry from a changelog"
+      (is (empty? (get-from-changelog change-log not-existing-entity-id))))
+    (testing "get (exact match) an existing entry from a changelog"
+      (let [ret (get-from-changelog change-log existing-entity-id)]
+        (is (= 1 (count ret)))
+        (is (= (first ret) (first change-log)))))
+    (testing "get (match only id) an existing entry from a changelog"
+      (let [ret (get-from-changelog change-log (select-keys existing-entity-id [:id]))]
+        (is (= 4 (count ret)))
+        (is (= ret (take 4 change-log)))))
+    (testing "get (match id and major version) an existing entry from a changelog"
+      (let [ret (get-from-changelog change-log (dissoc existing-entity-id :minor))]
+        (is (= 2 (count ret)))))
+    (testing "get (match id and minor version) an existing entry from a changelog"
+      (let [ret (get-from-changelog change-log (dissoc existing-entity-id :major))]
+        (is (= 2 (count ret)))))))
